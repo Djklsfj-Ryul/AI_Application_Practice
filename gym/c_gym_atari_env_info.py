1e3b5509384ddc946779de248c9fd184fd6939ac
Djklsfj_Ryul <102909120+Djklsfj-Ryul@users.noreply.github.com>
2023-09-06 15:48:07 +0900

    Annotation & Arcade Sample
    주석 추가
    Sample 다루기

--- gym/c_gym_atari_env_info.py
+++ gym/c_gym_atari_env_info.py
@@ -11,6 +11,7 @@ def observation_and_action_space_info():
     #####################
     print("*" * 80)
     print("[observation_space]")
+    #[0,255,(210,160,3),uint8] -> [최소, 최대, 3채널 2D 디멘션 어레이, 타입]
     print("env.observation_space: ", env.observation_space)
     print("env.observation_space.high:", env.observation_space.high)
     print("env.observation_space.low:", env.observation_space.low)
@@ -33,3 +34,31 @@ def observation_and_action_space_info():
 
 if __name__ == "__main__":
     observation_and_action_space_info()
+    for episode in range(3):
+        # Reset
+        # observation : State (단 t = 0)
+        observation, info = env.reset(seed=123, options={})
+
+        # S_t 가 완료된 상태인지를 확인하는 변수
+        done = False
+        episode_reward = 0.0
+        step = 0
+        while not done:
+            # Agent policy that uses the observation and info
+            action = env.action_space.sample()
+            # State 다음 time step
+            # terminated : 파괴 상태 (바로 종료)
+            # truncated  : 모든 스텝 완료
+            next_observation, reward, terminated, truncated, info = env.step(action)
+            step += 1
+
+            print(
+                "Step: {0}, Obs.: {1}, Action: {2}, Next Obs.: {3}, Reward: {4:5.2f}, Terminated: {5}, Truncated: {6}, Info: {7}".format(
+                    step, observation, action, next_observation, reward, terminated, truncated, info
+                ))
+
+            episode_reward += reward
+            observation = next_observation
+            done = terminated or truncated
+
+        print(f"Episode: {episode + 1}, Episode Reward: {episode_reward}")
